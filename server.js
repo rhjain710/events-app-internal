bfcb444b7dcdbefd68a109296d76394374f1f46b
ree <ree@gmail.com>
2020-11-19 06:40:55 +0000

    Added Team Name

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(bodyParser.json());
 // from a cloud data store
 const mockEvents = {
     events: [
-        { title: 'an event', id: 1, description: 'something really cool' },
-        { title: 'another event', id: 2, description: 'something even cooler' }
+        { title: 'A cloud event', id: 1, description: 'Something really cool' },
+        { title: 'Another cloud event', id: 2, description: 'Something even cooler' }
     ]
 };
 
